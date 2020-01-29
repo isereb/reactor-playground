9772700823172a090fdda5df7f8037525ced7d66
isereb <ruxxay.coder@gmail.com>
2020-01-29 01:33:50 +0000

    Increased work-flow complexity

--- src/main/java/com/isereb/projectreactor/playground/MyService.java
+++ src/main/java/com/isereb/projectreactor/playground/MyService.java
@@ -3,14 +3,14 @@ package com.isereb.projectreactor.playground;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.Disposable;
-import reactor.core.Disposables;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
 
 import javax.annotation.PostConstruct;
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 @Service
@@ -18,41 +18,48 @@ import java.util.concurrent.TimeUnit;
 @RequiredArgsConstructor
 public class MyService {
 
+    WebClient client = WebClient.create("http://localhost:8080");
+
     private final MyRepo repo;
     private final Scheduler scheduler =
-            Schedulers.newBoundedElastic(3, 1, "my-scheduler", 60);
+            Schedulers.newBoundedElastic(3, 1, "my-scheduler", 5);
+    private final Scheduler responseScheduler =
+            Schedulers.newBoundedElastic(3, 1, "resoibse", 5);
 
     @PostConstruct
     public void doSomeWork() {
         log.info("Hello from @PostConstruct");
-        Disposable.Composite disposable = Disposables.composite(
-                scheduler.createWorker()
-                        .schedulePeriodically(this::someWork, 0, 50, TimeUnit.MILLISECONDS),
-                scheduler.createWorker()
-                        .schedulePeriodically(this::someWork, 0, 50, TimeUnit.MILLISECONDS),
-                scheduler.createWorker()
-                        .schedulePeriodically(this::someWork, 0, 50, TimeUnit.MILLISECONDS));
+        Disposable disposable = Flux.just(1).delayElements(Duration.ofSeconds(2))
+                .log("scheduled")
+                .doOnNext(a -> someWork())
+                .subscribe();
         scheduler.schedule(disposable::dispose, 10, TimeUnit.SECONDS);
+
     }
 
     public Flux<String> getNames() {
         return repo.getAll()
-                .log()
+                .publishOn(responseScheduler)
+                .log("my-service")
                 .map(MyEntity::getName);
     }
 
     public Flux<Integer> getNumbers() {
         return repo.getAll()
-                .log()
+                .publishOn(responseScheduler)
+                .log("my-service")
                 .map(MyEntity::getNumber);
     }
 
     public void someWork() {
         try {
-            Thread.sleep(1000);
-            getNames()
-                    .log()
-                    .doOnNext(n -> log.info("Hello {} from {}", n, Thread.currentThread().getName()))
+            client.get()
+                    .uri("/v1/names")
+                    .retrieve()
+                    .bodyToFlux(String.class)
+                    .publishOn(scheduler)
+                    .log("web-client")
+                    .doOnNext(e -> log.info("Hello to {} from thread {}", e, Thread.currentThread().getName()))
                     .subscribe();
             Thread.sleep(1000);
         } catch (InterruptedException e) {
